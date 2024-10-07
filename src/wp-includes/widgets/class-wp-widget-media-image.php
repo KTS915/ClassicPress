43845afbd9f8778fa757577ebca71cfeecc07c40
Tim Kaye <KTS915@users.noreply.github.com>
2024-10-06 23:28:22 -0400

    Update class-wp-widget-media-image.php to save images correctly

--- src/wp-includes/widgets/class-wp-widget-media-image.php
+++ src/wp-includes/widgets/class-wp-widget-media-image.php
@@ -1,9 +1,25 @@
 <?php
 /**
- * Adds Image widget.
+ * Widget API: WP_Widget_Media_Image class
+ *
+ * @package ClassicPress
+ * @subpackage Widgets
+ * @since CP-2.3.0
+ */
+
+/**
+ * Core class that implements an image widget.
+ *
+ * @since 4.8.0
+ * @since CP-2.3.0
+ *
+ * Rebuilt to use PHP instead of JavaScript templates.
+ *
+ * @see WP_Widget
  */
 class WP_Widget_Media_Image extends WP_Widget {
-/**
+
+	/**
 	 * Constructor.
 	 *
 	 * @since 4.8.0
@@ -17,29 +33,18 @@ class WP_Widget_Media_Image extends WP_Widget {
 				'mime_type'   => 'image',
 			)
 		);
-/* 
- * DO NOT DELETE 
- * $this->l10n needs updating to avoid errors
- * 
-		$this->l10n = array_merge(
-			$this->l10n,
-			array(
-				'no_media_selected'          => __( 'No image selected' ),
-				'add_media'                  => _x( 'Add Image', 'label for button in the image widget' ),
-				'replace_media'              => _x( 'Replace Image', 'label for button in the image widget; should preferably not be longer than ~13 characters long' ),
-				'edit_media'                 => _x( 'Edit Image', 'label for button in the image widget; should preferably not be longer than ~13 characters long' ),
-				'missing_attachment'         => sprintf(
-					/* translators: %s: URL to media library. *
-					__( 'That image cannot be found. Check your <a href="%s">media library</a> and make sure it was not deleted.' ),
-					esc_url( admin_url( 'upload.php' ) )
-				),
-				/* translators: %d: Widget count. *
-				'media_library_state_multi'  => _n_noop( 'Image Widget (%d)', 'Image Widget (%d)' ),
-				'media_library_state_single' => __( 'Image Widget' ),
-			)
-		);
-*/	}
+	}
 
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @since CP-2.3.0
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
     public function widget( $args, $instance ) {
         echo $args['before_widget'];
 
@@ -75,6 +80,15 @@ class WP_Widget_Media_Image extends WP_Widget {
         echo $args['after_widget'];
     }
 
+	/**
+	 * Back-end widget form.
+	 *
+	 * @since CP-2.3.0
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
     public function form( $instance ) {
         $title     = ! empty( $instance['title'] ) ? $instance['title'] : '';
         $image_id  = ! empty( $instance['image_id'] ) ? $instance['image_id'] : 0;
@@ -83,39 +97,116 @@ class WP_Widget_Media_Image extends WP_Widget {
         $link_url  = ! empty( $instance['link_url'] ) ? $instance['link_url'] : '';
         $caption   = ! empty( $instance['caption'] ) ? $instance['caption'] : '';
         $alt_text  = ! empty( $instance['alt_text'] ) ? $instance['alt_text'] : '';
-
         ?>
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-        </p>
-        <div class="media-widget-preview media_image"><?php // gets "populated" added to class when image added ?>
-			<div class="attachment-media-view"><?php // replaced with <img class="attachment-thumb" src="http://localhost/tryfile/wp-content/uploads/2024/AWRDS24_Innovations_badge-300x170.png" draggable="false" alt="2024 Innovations in Technology Award Recipient"> when image added ?>
-				<button type="button" class="select-media button-add-media not-selected">Add Image</button>
-			</div>
-		</div>
-		<p class="media-widget-buttons">
-			<button type="button" class="button edit-media selected">Edit Image</button>
-			<button type="button" class="button change-media select-media selected">Replace Image</button>
-		</p>
-		<div class="media-widget-fields"></div><?php /* populated with the following when image added 
-        <p class="media-widget-image-link">
-			<label for="el0.5176951584933098_linkUrl">Link to:</label>
-			<input id="el0.5176951584933098_linkUrl" type="text" class="widefat link" value="" placeholder="https://" pattern="((\w+:)?\/\/\w.*|\w+:(?!\/\/$)|\/|\?|#).*">
-		</p> */ ?>
-        <?php
-    }
 
+		<div class="media-widget-control">
+			<fieldset>
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:' ); ?></label>
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+ 
+				<input class="widefat" id="<?php echo $this->get_field_id( 'image_id' ); ?>" name="<?php echo $this->get_field_name( 'image_id' ); ?>" type="hidden" data-property="image_id" value="<?php echo esc_attr( $image_id ); ?>">
+			</fieldset>
+
+			<fieldset>
+				<div class="media-widget-preview media_image">
+
+					<?php
+					if ( $image_id ) {
+						echo wp_get_attachment_image( $image_id, 'medium' );
+					}
+					?>
+
+				</div>
+						
+				<?php
+				if ( $image_id ) {
+				?>
+
+					<div class="media-widget-buttons">
+						<button type="button" class="button edit-media"><?php esc_html_e( 'Edit Image' ); ?></button>
+						<button type="button" class="button change-media select-media"><?php esc_html_e( 'Replace Image' ); ?></button>
+					</div>
+
+				<?php
+				} else {
+				?>
+
+					<div class="attachment-media-view">
+						<button type="button" class="select-media button-add-media"><?php esc_html_e( 'Add Image' ); ?></button>
+					</div>
+
+				<?php
+				}
+				?>
+
+			</fieldset>
+
+			<fieldset>
+				<label for="<?php echo $this->get_field_id('size'); ?>"><?php esc_html_e( 'Size:' ); ?></label>
+				<select class="widefat" id="<?php echo $this->get_field_id('size'); ?>" name="<?php echo $this->get_field_name('size'); ?>">
+
+					<?php
+					$sizes = get_intermediate_image_sizes();
+					foreach ( $sizes as $size_option ) {
+						if ( in_array( $size_option, array( '1536x1536', '2048x2048' ), true ) ) {
+							continue;
+						}
+						echo '<option value="' . esc_attr( $size_option ) . '"' . selected( $size, $size_option, false ) . '>' . esc_html( ucfirst( $size_option ) ) . '</option>';
+					}
+					?>
+
+				</select>
+			</fieldset>
+
+			<fieldset>
+				<label for="<?php echo $this->get_field_id( 'link_type' ); ?>"><?php esc_html_e( 'Link To:' ); ?></label>
+				<select class="widefat" id="<?php echo $this->get_field_id( 'link_type' ); ?>" name="<?php echo $this->get_field_name( 'link_type' ); ?>">
+					<option value="" <?php selected( $link_type, '' ); ?>><?php esc_html_e( 'None' ); ?></option>
+					<option value="file" <?php selected( $link_type, 'file' ); ?>><?php esc_html_e( 'Media File' ); ?></option>
+					<option value="custom" <?php selected( $link_type, 'custom' ); ?>><?php esc_html_e( 'Custom URL' ); ?></option>
+				</select>
+			</fieldset>
+
+			<fieldset>
+				<label for="<?php echo $this->get_field_id( 'link_url' ); ?>"><?php esc_html_e( 'Custom URL:' ); ?></label>
+				<input class="widefat" id="<?php echo $this->get_field_id( 'link_url' ); ?>" name="<?php echo $this->get_field_name( 'link_url' ); ?>" type="text" value="<?php echo esc_attr( $link_url ); ?>">
+			</fieldset>
+
+			<fieldset>
+				<label for="<?php echo $this->get_field_id( 'caption' ); ?>"><?php esc_html_e( 'Caption:' ); ?></label>
+				<textarea class="widefat" id="<?php echo $this->get_field_id( 'caption' ); ?>" name="<?php echo $this->get_field_name( 'caption' ); ?>"><?php echo esc_textarea( $caption ); ?></textarea>
+			</fieldset>
+
+			<fieldset>
+				<label for="<?php echo $this->get_field_id( 'alt_text' ); ?>"><?php esc_html_e( 'Alt Text:' ); ?></label>
+				<input class="widefat" id="<?php echo $this->get_field_id( 'alt_text' ); ?>" name="<?php echo $this->get_field_name( 'alt_text' ); ?>" type="text" value="<?php echo esc_attr( $alt_text ); ?>">
+			</fieldset>
+		</div>
+
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @since CP-2.3.0
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
     public function update( $new_instance, $old_instance ) {
         $instance = array();
         $instance['title']     = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
         $instance['image_id']  = ( ! empty( $new_instance['image_id'] ) ) ? absint( $new_instance['image_id'] ) : 0;
         $instance['size']      = ( ! empty( $new_instance['size'] ) ) ? sanitize_text_field( $new_instance['size'] ) : 'full';
         $instance['link_type'] = ( ! empty( $new_instance['link_type'] ) ) ? sanitize_text_field( $new_instance['link_type'] ) : '';
-        $instance['link_url']  = ( ! empty( $new_instance['link_url'] ) ) ? esc_url_raw( $new_instance['link_url'] ) : '';
+        $instance['link_url']  = ( ! empty( $new_instance['link_url'] ) ) ? sanitize_url( $new_instance['link_url'] ) : '';
         $instance['caption']   = ( ! empty( $new_instance['caption'] ) ) ? wp_kses_post( $new_instance['caption'] ) : '';
         $instance['alt_text']  = ( ! empty( $new_instance['alt_text'] ) ) ? sanitize_text_field( $new_instance['alt_text'] ) : '';
-
         return $instance;
     }
 }
